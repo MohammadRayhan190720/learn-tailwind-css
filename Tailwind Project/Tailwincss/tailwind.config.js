f94c8671a630f51f673d2bbcccc8f9815a14df89
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-07 22:59:12 +0600

    tailwind Practice

---
+++ Tailwind Project/Tailwincss/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    screens:{
+      sm:'420px',
+
+    },
+    colors:{
+      green:'#22a6b3',
+    },
+    fontFamily:{
+      sarif:['Noto Sans JP','sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
